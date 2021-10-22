29e5fe84bef8f8f906db7c736352a38af26ad5d9
davidlee <lidpwnz@gmail.com>
2021-10-22 13:56:56 +0600

    Init commit + описал модель цитаты

---
+++ source/api_v1/models.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from typing import List
+
+from django.db import models
+
+
+class BaseModel(models.Model):
+    created_at: datetime = models.DateTimeField(auto_now_add=True)
+    updated_at: datetime = models.DateTimeField(auto_now=True)
+
+
+class Quote(BaseModel):
+    STATUS_CHOICES: List[tuple] = [('New', 'New'), ('Moderated', 'Moderated')]
+
+    text: str = models.TextField()
+    author_name: str = models.CharField(max_length=150)
+    author_email: str = models.EmailField()
+    rating: int = models.IntegerField(default=0)
+    status: str = models.CharField(choices=STATUS_CHOICES, default='New')
